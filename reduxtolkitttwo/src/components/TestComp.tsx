e61b996fbe364bc732a6d906acda4317e6441e6d
Abdullah Al Mamun <abdullaalmamun100@gmail.com>
2024-06-27 14:38:18 +0800

    redextoolkittwoupdate

--- reduxtolkitttwo/src/components/TestComp.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { useAppSelector } from "../redux/hooks";
-
-export const TestComp = () => {
-  const count = useAppSelector((s) => s.counter);
-  return <h1>TestComp : {count}</h1>;
-};
